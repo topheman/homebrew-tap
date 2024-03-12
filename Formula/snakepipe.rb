ce4079b2f6990107926bc61536c59c7f21e82465
Christophe Rosset <tophe@topheman.com>
2024-03-12 23:49:03 +0100

    update snakepipe@2.1.0

--- Formula/snakepipe.rb
+++ Formula/snakepipe.rb
@@ -1,24 +1,27 @@
 class Snakepipe < Formula
   desc "Snake game based on stdin/stdout in rust"
   homepage "https://github.com/topheman/snake-pipe-rust"
-  version "2.0.0"
+  version "2.1.0"
   license "MIT"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/topheman/snake-pipe-rust/releases/download/v2.0.0/snakepipe-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "b979b5fc4e1cd4a00b1388c86f1edcff3db862158c7df5bfde14da55bfd2ab29"
+    url "https://github.com/topheman/snake-pipe-rust/releases/download/v2.1.0/snakepipe-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "766507e0efe425e765da7c77637d10cff51b73bc53c3c30864a0b2cf19af225d"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/topheman/snake-pipe-rust/releases/download/v2.0.0/snakepipe-aarch64-apple-darwin.tar.gz"
-    sha256 "222afb322c9e9e66840a12a04669654a9fbb0b385ca93f37683057a776d5615c"
+    url "https://github.com/topheman/snake-pipe-rust/releases/download/v2.1.0/snakepipe-aarch64-apple-darwin.tar.gz"
+    sha256 "f39bc0ed69f726e68be5af3789afa7a831390fd2fa4e58b83fa5a91c1d392410"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/topheman/snake-pipe-rust/releases/download/v2.0.0/snakepipe-x86_64-apple-darwin.tar.gz"
-    sha256 "68bd8d16037d859cd0f4016e2fba8aa4695d1e387c52ca2f0c4e8d863ca4e7a3"
+    url "https://github.com/topheman/snake-pipe-rust/releases/download/v2.1.0/snakepipe-x86_64-apple-darwin.tar.gz"
+    sha256 "3850aa0f4ed75bedadbb9c6dfdd7b0d5cc063937a4cd8f65d1cafff75942f945"
   end
 
   def install
     bin.install "snakepipe"
+    bash_completion.install "completions/bash/snakepipe"
+    fish_completion.install "completions/fish/snakepipe.fish"
+    zsh_completion.install "completions/zsh/_snakepipe"
   end
 
   test do
